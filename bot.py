9dea8b97ef55ea1e923180222e8f83d5fb7374cd
XboxBedrock <68715625+XboxBedrock@users.noreply.github.com>
2021-12-05 13:38:57 -0800

    Initial commit

---
+++ bot.py
@@ -0,0 +1,94 @@
+import discord
+import logging
+from subprocess import Popen, PIPE
+from discord.ext import commands
+from configLoader import Config
+import subprocess
+
+config = Config("config.json")
+bot = commands.Bot(command_prefix=config.prefix)
+
+
+@bot.command()
+async def status(ctx, bot="all"):
+    if bot != "all":
+        if not config.getBot(bot):
+            return await ctx.message.channel.send(embed=discord.Embed(title="Error", description="Bot not found", color=0xff0000))
+        cmdarray = ['pm2', 'show', bot]
+    else:
+        cmdarray = ['pm2', 'status']
+    if not config.hasPerms(ctx):
+        return
+    result = Popen(cmdarray, stdout=PIPE, stderr=PIPE,
+                   shell=True).communicate()
+
+    return await ctx.message.channel.send(embed=discord.Embed(title="Result", description="```\n" + result[0].decode('utf-8') + "```", color=0x00ff00))
+
+
+@bot.command()
+async def logs(ctx, bot):
+
+    if not config.hasPerms(ctx):
+        return
+    if not config.getBot(bot):
+        return await ctx.message.channel.send(embed=discord.Embed(title="Error", description="Bot not found", color=0xff0000))
+    result = Popen(['pm2', 'logs', bot, "--nostream", "--lines",
+                   "5000"], stdout=PIPE, stderr=PIPE, shell=True).communicate()
+    print(result[1].decode('utf-8')[-4096:])
+    print(result[1].decode('utf-8'))
+
+    await ctx.message.channel.send(embed=discord.Embed(title="Result", description="```\n" + result[0].decode('utf-8')[-4000:] + "```", color=0x00ff00))
+
+
+@bot.command()
+async def start(ctx, bot):
+
+    if not config.hasPerms(ctx):
+        return
+    if not config.getBot(bot):
+        return await ctx.message.channel.send(embed=discord.Embed(title="Error", description="Bot not found", color=0xff0000))
+    result = Popen(['pm2', 'start', bot], stdout=PIPE,
+                   stderr=PIPE, shell=True).communicate()
+
+    await ctx.message.channel.send(embed=discord.Embed(title="Result", description="```\n" + result[0].decode('utf-8') + "```", color=0x00ff00))
+
+
+@bot.command()
+async def stop(ctx, bot):
+    if not config.hasPerms(ctx):
+        return
+    if not config.getBot(bot):
+        return await ctx.message.channel.send(embed=discord.Embed(title="Error", description="Bot not found", color=0xff0000))
+    result = Popen(['pm2', 'stop', bot], stdout=PIPE,
+                   stderr=PIPE, shell=True).communicate()
+
+    await ctx.message.channel.send(embed=discord.Embed(title="Result", description="```\n" + result[0].decode('utf-8') + "```", color=0x00ff00))
+
+
+@bot.command()
+async def restart(ctx, bot):
+    if not config.hasPerms(ctx):
+        return
+    if not config.getBot(bot):
+        return await ctx.message.channel.send(embed=discord.Embed(title="Error", description="Bot not found", color=0xff0000))
+    result = Popen(['pm2', 'restart', bot], stdout=PIPE,
+                   stderr=PIPE, shell=True).communicate()
+
+    await ctx.message.channel.send(embed=discord.Embed(title="Result", description="```\n" + result[0].decode('utf-8') + "```", color=0x00ff00))
+
+
+@bot.command()
+async def pull(ctx, bot, commit="latest"):
+    if commit == "latest":
+        cmdargs = ['pm2', 'pull', bot]
+    else:
+        cmdargs = ['pm2', 'pull', bot, commit]
+    if not config.hasPerms(ctx):
+        return
+    if not config.getBot(bot):
+        return await ctx.message.channel.send(embed=discord.Embed(title="Error", description="Bot not found", color=0xff0000))
+    result = Popen(cmdargs, stdout=PIPE, stderr=PIPE, shell=True).communicate()
+
+    await ctx.message.channel.send(embed=discord.Embed(title="Result", description="```\n" + result[0].decode('utf-8') + "```", color=0x00ff00))
+
+bot.run(config.token)
---
+++ configLoader.py
@@ -0,0 +1,28 @@
+import json
+import discord
+
+
+class Config:
+    def __init__(self, configFile):
+        self.configFile = json.load(open(configFile))
+        self.token = self.configFile["token"]
+        self.bots = self.configFile["bots"]
+        self.prefix = self.configFile["prefix"]
+        self.roles = self.configFile["roles"]
+        self.users = self.configFile["users"]
+
+    def getBot(self, bot):
+        return bot in self.bots.keys()
+
+    def hasPerms(self, ctx):
+        for i in self.users:
+            print(ctx.author.id)
+            print(i)
+            if str(ctx.author.id) == i:
+                return True
+        for j in self.roles:
+            role = discord.utils.find(
+                lambda r: r.id == j, ctx.message.server.roles)
+            if role in ctx.message.author.roles:
+                return True
+        return False
